8588cb9c28f43e115fca0f808e776cbdb9542685
Huu Tran <20810212@student.hcmus.edu.vn>
2023-04-11 12:37:28 +0700

    first 2 commit

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Home</span>
+    <a href="https://netbanbe.com" target="_blank" class="text-blue-500">
+      NetBanBe
     </a>
   </div>
 </template>
