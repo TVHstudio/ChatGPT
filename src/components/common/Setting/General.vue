14d1edda823b71b658e33bba1e9def0f78f31793
Huu Tran <20810212@student.hcmus.edu.vn>
2023-04-12 14:39:36 +0700

    add vn2

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -54,12 +54,12 @@ const themeOptions: { label: string; key: Theme; icon: string }[] = [
 ]
 
 const languageOptions: { label: string; key: Language; value: Language }[] = [
-  { label: 'English', key: 'en-US', value: 'en-US' },
+  { label: 'English',    key: 'en-US', value: 'en-US' },
   { label: 'Tiếng Việt', key: 'vi-VN', value: 'vi-VN' },
-  { label: '简体中文', key: 'zh-CN', value: 'zh-CN' },
-  { label: '繁體中文', key: 'zh-TW', value: 'zh-TW' },
-  { label: '한국어',   key: 'ko-KR', value: 'ko-KR' },
-
+  { label: '简体中文',    key: 'zh-CN', value: 'zh-CN' },
+  { label: '繁體中文',    key: 'zh-TW', value: 'zh-TW' },
+  { label: '한국어',      key: 'ko-KR', value: 'ko-KR' },
+  
 ]
 
 function updateUserInfo(options: Partial<UserInfo>) {
